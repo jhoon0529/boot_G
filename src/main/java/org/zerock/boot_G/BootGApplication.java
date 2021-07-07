9f636364b4a524fbad925ca00c9eed57e8bb7ddd
jhoon0529 <jhoon0529@icloud.com>
2021-07-07 22:05:17 +0900

    spring-boot gradle

---
+++ src/main/java/org/zerock/boot_G/BootGApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.boot_G;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/boot_G/BootGApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.boot_G;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
